a5435ee64f6f1929684ae90d9d81e3aac0278c9d
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-03-17 13:37:29 +0000

    Modification of task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main (void)
+
+/*
+ * This is a c program that prints to the standard output
+ * with the use of puts()
+ *
+ * The main() returns 0 (success)
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
