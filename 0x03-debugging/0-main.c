9c9e0ed9b145a9076b793ab08bd7e959146f854d
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-03-25 02:50:31 +0000

    Completion of task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - tests function
+ * that prints if integer
+ * is positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* variable declaration */
+	int i;
+
+	/* variable initialization */
+	i = 0;
+
+	/* function call */
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+# ALX: 0x03-debugging Project.
+* 0. Multiple mains
+* 1. Like, comment, subscribe
+* 2. 0 > 972?
+* 3. Leap year
