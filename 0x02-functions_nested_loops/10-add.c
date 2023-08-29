a21a7812ced3fa776a150566d5b3557d5ed5a403
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-08-29 15:55:03 +0000

    Task 10 completed!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds to numbers
+ *
+ * @a: int a
+ * @b: int b
+ *
+ * Return: integer.
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
