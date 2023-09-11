5254c03678a631eefbf7efd339c976fb7924923b
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-09-11 21:44:05 +0000

    Task 0 Completed!

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - contains properties of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: It stores the datatype of
+ * all the above properties of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
