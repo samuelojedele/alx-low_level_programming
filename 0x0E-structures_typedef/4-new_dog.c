3752e41f6f03c8d76800efcb3e10f1e97a2cf089
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-09-12 00:47:05 +0000

    Debugging!

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,16 +21,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->name = malloc(sizeof(name));
+
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	strcpy(new_dog->name, name);
+
 	new_dog->owner = malloc(sizeof(owner));
 
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 
-	strcpy(new_dog->name, name);
 	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
