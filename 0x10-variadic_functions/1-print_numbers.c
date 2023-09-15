88d2dfe9c926f9ba3bdb61eb56a8287df3f38e14
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-09-15 19:45:28 +0000

    Task 1 Correction!

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,6 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
+	if (separator == NULL)
+		return;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
